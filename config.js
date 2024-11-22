88413eb20ca76b2de2b58866e89725aa423f9f2a
浪子 <jkjoy@live.cn>
2024-11-22 08:14:31 +0800

    Update config.js
    使用env

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
     server: {
+        port: process.env.PORT || 3000,
         host: '0.0.0.0'
+    },
+    email: {
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT, 10) || 587,
+        secure: process.env.SMTP_SECURE === 'true',
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
     },
     security: {
         verificationCodeExpiry: 5 * 60 * 1000, // 5分钟
